144a9b6be730ea3f0f8a4d5d875a6d397888ff40
Umbrita <128509112+Umbrita@users.noreply.github.com>
2024-03-04 19:10:04 +0100

    Revert "prueba content.js"
    This reverts commit 1c8b21da87264c4b200c9ddfe36aa36b6c4cf76d.

--- assets/js/contact.js
+++
@@ -1,9 +0,0 @@
-function showPopup() {
-
-    const mensaje = document.querySelector('.textarea[id="message"].value'); 
-    const popup = document.querySelector(".popup");
-
-    popup.hide();
-    console.log(mensaje.value);
-
-}
