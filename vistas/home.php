16d50416d7438f7b4313ce641bbab0161c031c0c
Umbrita <128509112+Umbrita@users.noreply.github.com>
2024-04-12 19:33:15 +0200

    comprobacion de si el mensaje esta vacio

--- vistas/home.php
+++ vistas/home.php
@@ -42,8 +42,8 @@
 
         <div class="home__chatbot">
             <div id="chat-container">
-            <div id="chat-conversation"></div>
-        </div>
+                <div id="chat-conversation"></div>
+            </div>
             <div class="chat-input">
                 <input type="text" id="user-input" placeholder="Type your message...">
                 <button id="send-button"><img src="./assets/img/send.png" alt=""></button>
@@ -58,39 +58,43 @@
             const system_prompt = "Imagina que eres un  experto en normativa y legislación de drones y deseas asegurarte de que el usuario cumpla con todas las regulaciones y normativas vigentes para el manejo y vuelo de drones. Proporcionarás respuestas claras y precisas sobre las regulaciones actuales, zonas restringidas, y te dará consejos avanzados para volar de manera segura y legal. Si el usuario pregunta o habla algo fuera de este contexto, pide al usuario que por favor, se ciña al contexto. Por favor, no repitas el contexto que te he dado, el usuario no necesita saber esta información. Solo puedes contestar a las preguntas en Español.";
             const userInput = document.getElementById('user-input').value;
             document.getElementById('user-input').value = '';
-            appendMessage('user', userInput);
+            if (userInput != "") {
+                appendMessage('user', userInput);
 
-            try {
-                async function query(data) {
-                    const response = await fetch(
-                        "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1",
-                        {
-                            headers: { 
-                                        Authorization: "Bearer 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",
-                                        'Content-Type': 'application/json' 
-                                    },
-                            method: "POST",
-                            body: JSON.stringify(data),
-                        }
-                    );
-                    const answer = await response.json();
-                    appendMessage('bot', answer[0].generated_text);
-                }
+                try {
+                    async function query(data) {
+                        const response = await fetch(
+                            "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1",
+                            {
+                                headers: {
+                                    Authorization: "Bearer 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",
+                                    'Content-Type': 'application/json'
+                                },
+                                method: "POST",
+                                body: JSON.stringify(data),
+                            }
+                        );
+                        const answer = await response.json();
+                        appendMessage('bot', answer[0].generated_text);
+                    }
 
-                query({ "inputs": "<s>[INST] " + system_prompt + "/n" + userInput + " [/INST]", 
+                    query({
+                        "inputs": "<s>[INST] " + system_prompt + "/n" + userInput + " [/INST]",
                         "parameters": {
-                            "temperature":0.8, 
-                            "min_length":32, 
-                            "max_length":10000, 
-                            "top_p":0.9, 
-                            "max_new_tokens":512, 
-                            "return_full_text":false
-                        }}).then((response) => {
-                    console.log(JSON.stringify(response));
-                });
+                            "temperature": 0.8,
+                            "min_length": 32,
+                            "max_length": 10000,
+                            "top_p": 0.9,
+                            "max_new_tokens": 512,
+                            "return_full_text": false
+                        }
+                    }).then((response) => {
+                        console.log(JSON.stringify(response));
+                    });
 
-            } catch (error) {
-                console.error('Error:', error);
+                } catch (error) {
+                    console.error('Error:', error);
+                }
             }
         }
 
